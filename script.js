4965428c401807e29d97fecca8d245e07a80a276
Veerash Palanichamy <veerashp05@gmail.com>
2019-02-21 00:08:00 -0500

    finished detection

--- script.js
+++ script.js
@@ -2,36 +2,65 @@ let pressed = false;
 let points = [];
 let c;
 let wb;
+let guess;
+//"http://127.0.0.1:8887/data.json"
+function preload(){
+    wb = loadJSON("http://127.0.0.1:8887/data.json");
+}
+function sigmoid(x){
+    return math.dotDivide(1,math.add(1,math.exp(math.dotMultiply(-1,x))))
+}
+function calculate(a){
+    for(var i=0;i<wb[0].length;i++){
+        a=sigmoid(math.add(math.multiply(math.matrix(wb[0][i]),a),math.matrix(wb[1][i])));
+    }
+    return a;
+}
 
-function setup(){
-    wb = $.getJSON( "http://127.0.0.1:8887/data.json", function() {
-        console.log( "success" );
-    })      
+function setup(){     
     createCanvas(500,500);
     pixelDensity(1);
-    // let button = createButton("Reset");
-    // button.mousePressed(()=>{
-    //     background(0);
-    //     points=[];
-    // });
+    let button = createButton("Reset");
+    button.mousePressed(()=>{
+        background(0);
+        guess.html("Draw a digit");
+        points=[];
+    });
+    button.position(200,510);
+    button.size(150,50);
     background(0);
     stroke(255);
     noFill();
-    strokeWeight(10);
+    strokeWeight(25);
+
+    let text = createP("The computer thinks its a:")
+    text.position(600,100);
+    text.style('font-size','40px');
+    guess = createP("Draw a digit");
+    guess.position(750,200);
+    guess.style('font-size','40px');
 }
 function mousePressed(){
-    pressed=true;
+    if(mouseX<500&mouseY<500){
+        pressed=true;
+    }else{
+        pressed=false;
+    }
     //beginShape();
 }
 function mouseReleased(){
     pressed=false;
+    if(mouseX>500||mouseY>500){
+        return false;
+        points=[];
+    }
     points=[];
     let img = createImage(500,500);
     loadPixels();
     img.loadPixels();
     for(var i=0;i<height;i++){
         for(var j=0;j<width;j++){
-            let index=(i+j*width)*4;
+            let index=(j+i*width)*4;
             img.pixels[index]=pixels[index];
             img.pixels[index+1]=pixels[index+1];
             img.pixels[index+2]=pixels[index+2];
@@ -43,36 +72,38 @@ function mouseReleased(){
     updatePixels();
     img.updatePixels();
     img.resize(28,28);
+    image(img,0,0);
+    //let temp = getData(img);
+    
     //image(img, 100,100);
     //img.style("border-style","solid");
     //img.loadPixels();
-    img.loadPixels();
-    //console.log(img.pixels);
+    //img.loadPixels();
     //console.log(getData(img));
 
 
-    //data writer
-    // let writer = createWriter('sample.txt');
-    // let temp = getData(img);
-    // for(var i=0;i<temp.length;i++){
-    //     if((i+1)%img.width!=0){
-    //         writer.print("");
-    //     }
-    //     writer.print(temp[i],end=',');
-    // }
-    // writer.close();
-
-    
-    //getData();
+    let temp = math.matrix(getData(img)).reshape([784,1]);
+    let result = calculate(temp);
+    let highest = 0;
+    let index;
+    for(var i=0;i<result.size()[0];i++){
+        if(result.get([i,0])>highest){
+            highest = result.get([i,0]);
+            index=i;
+        }
+    }
+    guess.html(str(index));
+    console.log(result);
+    console.log("The number is",index);             
 }
 function getData(img){
     let outArr = []
     img.loadPixels();
     for(var i=0;i<img.height;i++){
         for(var j=0;j<img.width;j++){
-            let index=(i+j*img.width)*4;
+            let index=(j+i*img.width)*4;
             //console.log(img.pixels[index]);
-            outArr.push((0.3 * img.pixels[index]) + (0.59 * img.pixels[index+1]) + (0.11 * img.pixels[index+2]));
+            outArr.push((0.299 * img.pixels[index]) + (0.587 * img.pixels[index+1]) + (0.114 * img.pixels[index+2]));
         }
     }
     return outArr;
@@ -80,7 +111,7 @@ function getData(img){
 
 
 function draw(){
-    if(pressed){
+    if(pressed==true){
         points.push([mouseX,mouseY]);
         beginShape();
         for(let i of points){
